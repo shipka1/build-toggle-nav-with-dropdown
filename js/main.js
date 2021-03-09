6b00a1bd4d94c2e53335eae0f326dac9dc72ceab
Shipka1 <nicholasshipka@gmail.com>
2021-03-08 22:18:48 -0700

    finished in-class nav build

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-// js goes here
+const toggle = document.querySelector('.menu-icons');
+
+toggle.addEventListener('click', () =>{
+    document.querySelector('nav').classList.toggle('show-nav');
+});
